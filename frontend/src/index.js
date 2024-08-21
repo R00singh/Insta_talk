1b6df514f158717076656f6dc856d1d6aed6b8af
R00singh <141959237+R00singh@users.noreply.github.com>
2024-08-21 19:32:45 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="insta-talk-gl8jvw3qk-rupesh-singhs-projects.vercel.app"
+export const BASE_URL="https://insta-talk.vercel.app/"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
